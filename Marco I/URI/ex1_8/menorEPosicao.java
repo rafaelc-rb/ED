3f17765133f45c7425a9d64f08f150ff4b1f3aa0
Rafael <rafaelcrd.ribeiro@gmail.com>
2021-07-14 14:52:11 -0300

    menorEPosicao implementado
    8º exercício do URI

--- Marco I/URI/ex1_8/menorEPosicao.java
+++ Marco I/URI/ex1_8/menorEPosicao.java
@@ -6,9 +6,30 @@ import java.util.Scanner;
 public class menorEPosicao {
     public static void main(String[] args) throws IOException{
         Scanner in = new Scanner(System.in);
-        
-        System.out.println("Not Yet Implemmented!");
+        int n;
+        int menor = 0;
+        int p = 0;
 
+        n = in.nextInt();
+        int v[] = new int[n];
+        in.nextLine();
+
+        String nmrs[] = in.nextLine().split(" ");       
+
+        for(int i = 0; i < v.length; i++){
+            v[i] = Integer.parseInt(nmrs[i]);
+
+            if(i == 0){
+                menor = v[i];
+                p = i;
+            }else if(menor > v[i]){
+                menor = v[i];
+                p = i;
+            }
+        }
+
+        System.out.println("Menor valor: " + menor);
+        System.out.println("Posicao: " + p);
         in.close();
     }
 }
