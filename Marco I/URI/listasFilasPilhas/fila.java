b894a180f4ea5d266d7b84b6ac381c173c191e67
Rafael <rafaelcrd.ribeiro@gmail.com>
2021-08-03 23:06:16 -0300

    fila implemented

--- Marco I/URI/listasFilasPilhas/fila.java
+++ Marco I/URI/listasFilasPilhas/fila.java
@@ -1,5 +1,38 @@
 package listasFilasPilhas;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class fila {
- 
+    public static void main(String[] args) throws IOException {
+        Scanner in = new Scanner(System.in); 
+        int size;
+        int delSize;
+
+        ArrayList<Integer> queue = new ArrayList<>();
+
+        size = in.nextInt();
+
+        for (int i = 0; i < size; i++) {
+            queue.add(in.nextInt());
+        }
+
+        delSize = in.nextInt();
+
+        for(int i = 0; i < delSize; i++){
+            int remove = in.nextInt();
+            queue.remove((Object) remove);
+        }
+
+        for(int i = 0; i < (size - delSize); i++){
+            if(i == 0)
+                System.out.print(queue.get(i));
+            else{
+                System.out.print(" " + queue.get(i));
+            }
+        }
+        System.out.println();;
+        in.close();
+    }
 }
