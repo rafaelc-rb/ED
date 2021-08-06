7b5a65778237de9fc2ef7804a4eec1e5d3fb9072
Rafael <rafaelcrd.ribeiro@gmail.com>
2021-08-05 22:38:19 -0300

    trilhos e cartas implemented
    + reorganização do repositório

--- Marco I/URI/listasFilasPilhas/jogandoCartasFora.java
+++ Marco I/URI/listasFilasPilhas/jogandoCartasFora.java
@@ -1,5 +1,67 @@
 package listasFilasPilhas;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.Stack;
+
 public class jogandoCartasFora {
-    
+    public static void main(String[] args) throws IOException {
+        Scanner in = new Scanner(System.in);
+        ArrayList<Integer> list = new ArrayList<>();
+        ArrayList<Integer> forwent = new ArrayList<>();
+        Stack<Integer> stack = new Stack<>();
+
+        int pfirst;
+        int numList;
+
+        do {
+            numList = in.nextInt();
+            if (numList != 0) {
+                list.add(numList);
+            }
+        } while (numList != 0);
+
+        for (Integer num : list) {
+            for (int i = num; i > 0; i--) {
+                stack.push(i);
+            }
+
+            while (stack.size() > 1) {
+                pfirst = stack.peek();
+                forwent.add(pfirst);
+                stack.pop();
+                pfirst = stack.peek();
+                stack.add(0,pfirst);
+                stack.pop();
+            }
+            System.out.print("Discarded cards: ");
+            for(int i = 0; i < forwent.size();i++){
+                System.out.print(forwent.get(i));
+                int d = i;
+                d++;
+                if(d == forwent.size()){
+                    System.out.println();
+                }else{
+                    System.out.print(", ");
+                }
+                
+            }
+            System.out.print("Remaining card: ");
+            for(int i = 0; i < stack.size();i++){
+                System.out.print(stack.get(i));
+                int d = i;
+                d++;
+                if(d == stack.size()){
+                    System.out.println();
+                }else{
+                    System.out.print(", ");
+                }
+                
+            }
+            stack.clear();
+            forwent.clear();
+        }
+        in.close();
+    }
 }
--- Marco I/URI/listasFilasPilhas/trilhos.java
+++ Marco I/URI/listasFilasPilhas/trilhos.java
@@ -1,5 +1,72 @@
 package listasFilasPilhas;
 
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.Stack;
+
 public class trilhos {
+    public static void main(String[] args) throws IOException {
+        Scanner in = new Scanner(System.in);
+
+        int savedNum, num, vagao, cont;
+        String vagaoStr, numStr;
+        cont = 0;
+            do{
+                numStr = in.nextLine();
+                savedNum = Integer.parseInt(numStr);
+                if(numStr.equals("0")){
+                    break;
+                }
+                
+                
+                if(cont == 0){
+                    cont++;
+                }else{
+                    System.out.println();
+                }
     
+            do{
+                vagaoStr = in.nextLine();
+                
+                String v[] = new String[savedNum];
+                Stack<Integer> stack = new Stack<>();
+                
+                if(vagaoStr.equals("0")){
+                    break;
+                }
+                
+                v = vagaoStr.split(" ");
+                
+                num =0;
+                vagao = Integer.parseInt(v[num]);
+                
+                for (int i = 1; i <= savedNum; i++) {
+                    stack.push(i);
+                
+                
+                while(!stack.empty() && stack.lastElement() == vagao){
+                   num++;
+                   if(num < savedNum){
+                       vagao =Integer.parseInt(v[num]);
+                   }
+                    stack.pop();
+                }
+                
+                }
+                
+                if(stack.empty()){
+                    System.out.println("Yes");
+                    //System.out.println(pilhaV);
+                }else if(!stack.empty()){
+                    System.out.println("No");
+                    //System.out.println(pilhaV);
+                }
+                
+            } while (!vagaoStr.equals("0"));
+        }while(!numStr.equals("0"));
+
+        System.out.println();
+        
+        in.close();
+    }
 }
